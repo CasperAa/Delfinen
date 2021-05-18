41113ea6801fe2e01ba740140cb5e5790b7891e2
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2021-05-18 12:06:54 +0200

    Amanda

--- src/Members/AddMember.java
+++ src/Members/AddMember.java
@@ -1,18 +1,24 @@
 package Members;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.PrintWriter;
 
 //@Amanda
 public class AddMember {
-    public void AddMember() {
+    public void AddMember(String name,String birthdate,String memberStatus,String memberGroup,String memberType,String telephoneNo, String email, String startDate) {
+        String ID;
+
+
         try {
             File membersFile = new File("Files/Members");
-            PrintWriter out = new PrintWriter(membersFile);
+            FileWriter fw = new FileWriter("Files/Members",true);   //Filen bliver ikke overwrited.
+            PrintWriter pw = new PrintWriter(membersFile);
 
-            out.println();
-
-            out.close();
+            pw.println(name + ";" + ID + ";" + birthdate + ";" + memberStatus + ";" + memberGroup + ";" + memberType +
+                    ";" + telephoneNo + ";" + email + ";" + startDate);   //Medlemmet skal tilføjes til filen
+            pw.flush();     //Kan ikke forkare, hvad der sker her
+            pw.close();     //Handlongen sker rent faktisk
             System.out.println("Medlem blev tilføjet");
         }
         catch (Exception e){
