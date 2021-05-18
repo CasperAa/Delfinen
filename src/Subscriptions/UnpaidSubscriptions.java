42fc08c35a2bedf3acb45881cf3a7f41d2d28e6d
Casper <casper.aakviststeengaard@gmail.com>
2021-05-18 15:01:15 +0200

    Tilføjet restance

--- src/Subscriptions/UnpaidSubscriptions.java
+++ src/Subscriptions/UnpaidSubscriptions.java
@@ -6,6 +6,7 @@ public class UnpaidSubscriptions {
 
     public static void paymentOverview (){
         System.out.println("Disse medlemmer er i restance:");
+
         for(Member member : Member.getMemberList()){
             if (!member.isHasPayed()){
                 System.out.println("\n"+ member.getID() + ": " + member.getName() + " - " + member.getMemberType() + "\n      "
