2dd75f67618e47fd67713534f0ca544c1655de2f
Casper <casper.aakviststeengaard@gmail.com>
2021-05-18 13:03:00 +0200

    Tilføjet Kontingent

--- src/Subscriptions/Revenue.java
+++ src/Subscriptions/Revenue.java
@@ -2,19 +2,21 @@ package Subscriptions;
 import Members.Members;
 
 public class Revenue {
-    int junior = 1000;
-    int senior = 1600;
+    double junior = 1000.00;
+    double senior = 1600.00;
     double sixtyDiscount = 0.25;
-    int passive = 500;
+    double passive = 500.00;
+    double sixty = 60.00;
 
 
 
     public long yearlyRevenue (){
-        long yearlyIncome = 0;
-
+        double yearlyIncome = 0.00;
 
         for (Members member : memberList){
-            if ()
+            if (Members.getMembertype.equals("Junior")){
+                yearlyIncome + junior;
+            } else if ( Members.getMemberType.equals("Senoir") && )
 
         }
 
