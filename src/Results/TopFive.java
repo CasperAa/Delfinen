e0cf1d5d7d7b8a2d35808abfbd1ec980d6a7b093
Casper <casper.aakviststeengaard@gmail.com>
2021-05-21 11:48:26 +0200

    Top5 ting

--- src/Results/TopFive.java
+++ src/Results/TopFive.java
@@ -1,4 +1,20 @@
 package Results;
 
+import java.io.File;
+
 public class TopFive {
+
+    public static void main(String[] args) {
+        fileReader();
+    }
+    public static void fileReader (){
+        File [] memberFiles = new File("src/Files/membersResults").listFiles();
+
+        for(File currentFile: memberFiles) {
+
+            System.out.println(currentFile.getName());
+
+        }
+
+    }
 }
